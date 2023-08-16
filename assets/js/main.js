fa2f0097d88f9765a62c7ba3cb39e18504008652
Nuri Demir <nuridemir34@hotmail.com>
2023-08-16 16:41:26 +0300

    added hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,16 @@
         loaderContainer.style.display = 'none';
     });
 })();
+
+
+const hamburgerOpenBtn = document.querySelector('.hamburger-open-btn')
+const hamburgerCloseBtn = document.querySelector('.hamburger-close-btn')
+
+hamburgerOpenBtn.addEventListener('click', () => {
+    document.querySelector("header nav").style.right = "0%";
+    document.querySelector("header nav").style.transition = "250ms";
+})
+hamburgerCloseBtn.addEventListener('click', () => {
+    document.querySelector("header nav").style.right = "-100%";
+    document.querySelector("header nav").style.transition = "250ms";
+})
